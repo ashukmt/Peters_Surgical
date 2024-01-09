7c8cd0c8c6d99be185dd934377d447dea92c48f2
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2024-01-10 03:10:20 +0530

    working

--- peters_sergical/src/App.js
+++ peters_sergical/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import SideSec from './components/SideSec';
+import { Flex } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
+     
+      {/* <Home/> */}
+      <Flex>
+      <SideSec/>
       <Home/>
+      </Flex>
     </div>
   );
 }
